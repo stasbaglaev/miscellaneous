bd8d4245ad431ba949540e89bdcbed29d30cd0b9
stasbaglaev <stasbaglaev@yandex.ru>
2022-09-04 13:19:22 +0300

    Added Sound file (book.shildt)

---
+++ src/main/java/book/shildt/chapter2/Sound.java
@@ -0,0 +1,15 @@
+package book.shildt.chapter2;
+
+public class Sound {
+
+    public static void main(String[] args) {
+
+        double dist;
+
+        dist = (1100*7.2)/2; //
+
+        System.out.println("Расстояние до объекта " + dist + " футов");
+
+    }
+
+}
