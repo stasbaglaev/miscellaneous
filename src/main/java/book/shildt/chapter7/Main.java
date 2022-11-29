ffe4f4fe852614e990e914c7aef89c9594636256
stasbaglaev <stasbaglaev@yandex.ru>
2022-11-29 22:38:00 +0300

    Added Main files (book.shildt)

---
+++ src/main/java/book/shildt/chapter7/Main.java
@@ -0,0 +1,16 @@
+package book.shildt.chapter7;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Human{
+Object object;
+
+    public Human() {
+        super();
+
+    }
+}
