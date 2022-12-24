fbf4deda20cffad3566cc97fcfbebe6fe06c3ba7
stasbaglaev <stasbaglaev@yandex.ru>
2022-12-24 20:52:36 +0300

    Added FunctionalInterface (book.shildt)

---
+++ src/main/java/book/shildt/chapter10/functionalinterface/ThirdVariant.java
@@ -0,0 +1,15 @@
+package book.shildt.chapter10.functionalinterface;
+
+public class ThirdVariant {
+//    public static void main(String[] args) {
+//        double d = (ThirdVariant::method(10));
+//    }
+//
+//    private static double method(double operand) {
+//
+//        return operand + 1;
+//
+//    }
+}
+
+
---
+++ src/main/java/book/shildt/chapter10/functionalinterface/TwoVariant.java
@@ -0,0 +1,10 @@
+package book.shildt.chapter10.functionalinterface;
+
+import java.util.function.DoubleUnaryOperator;
+
+public class TwoVariant {
+//    public static void main(String[] args) {
+//        System.out.println((DoubleUnaryOperator) operand -> operand + 1);
+//    }
+}
+
