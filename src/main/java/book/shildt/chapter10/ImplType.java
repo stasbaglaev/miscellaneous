6d1b971bf729987f6b6c1ca0eda501a2dba66457
stasbaglaev <stasbaglaev@yandex.ru>
2022-12-25 13:13:22 +0300

    Added files (book.shildt)

---
+++ src/main/java/book/shildt/chapter10/ImplType.java
@@ -0,0 +1,9 @@
+package book.shildt.chapter10;
+
+public class ImplType {
+    public static void main(String[] args) {
+        String str = "100$";
+
+        System.out.println(Integer.parseInt(str));
+    }
+}
